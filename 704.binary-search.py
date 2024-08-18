4edabf92da4e17f121eac0668041264041b5b51d
Luiz Salazar <luizsalazar87@gmail.com>
2024-08-17 19:30:34 -0500

    704.binary-search.py

--- 704.binary-search.py
+++ 704.binary-search.py
@@ -55,5 +55,13 @@ class Solution:
         right = len(nums) - 1
         while(left <= right):
             mid = (left + right) //2
+            if nums[mid] == target:
+                return mid
+            if target > nums[mid]:
+                left = mid + 1
+            else:
+                right = mid -1
+
+        return -1
 # @lc code=end
 
